c483e26c095406083cdab1bc5fda485a5f078522
Franck M <franckisys@gmail.com>
2019-08-20 11:57:04 +0200

    Update

--- add_subnets.sh
+++ add_subnets.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 usage() {
     cat >&2 <<EOF
-Usage: $0 <Last IP address/xx> <debug yes/no>
+Usage: $0 <Subnet address/xx or Subnets address/xx,address/xx> <debug yes/no>
 Add subnets to access dird-phoned with physical phone
 EOF
-	exit 1
+        exit 1
 }
 
 if [ "$#" -ne 2 ]; then
    usage
 fi
 
-echo "1/3 - Create file"
-
+echo "Create/Update file ${1} ${2}"
+DEBUG="${2}"
+IP="${1}"
 touch /etc/wazo-dird-phoned/conf.d/010-subnets.yml
-cat > /etc/wazo-dird-phoned/conf.d/010-subnets.yml <<'EOF'
-debug: $2
+cat > /etc/wazo-dird-phoned/conf.d/010-subnets.yml <<EOF
+debug: ${DEBUG}
 rest_api:
-  authorized_subnets: ["$1"]
+  authorized_subnets: ["${IP}"]
 EOF
 
 echo "Done"
