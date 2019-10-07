e90b87103a44cadf334104196dfb0fe91b5d458f
Franck M <franckisys@gmail.com>
2019-10-07 20:11:21 +0200

    global update

--- add_subnets.sh
+++ add_subnets.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
-usage() {
-    cat >&2 <<EOF
-Usage: $0 <Subnet address/xx or Subnets address/xx,address/xx> <debug yes/no>
-Add subnets to access dird-phoned with physical phone, works only with UCEngine >= 19.11
-EOF
-        exit 1
+
+bye() {
+whiptail --title "Hello Wazo HELP / IP PhoneBook" --msgbox "Please reset or re-provision your devices\nHave a nice day :-) Wazo Support." 10 60
 }
 
-if [ "$#" -ne 2 ]; then
-   usage
+autorize_subnets() {
+IP_authorize=$(whiptail --title "Hello Wazo HELP / IP Authorize Subnet" --inputbox "Please insert Public IP to authorize\nExemple :\nFor single IP : 10.10.10.10\nFor single IP Subnet : 10.10.10.10/24\nFor multiple IP or Subnets : 10.10.10.10/24, 1.1.1.1" 13 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+{
+    echo -e "XXX\n0\nAdd IP Please Wait... \nXXX"
+    touch /etc/wazo-phoned/conf.d/010-subnets.yml
+    cat > /etc/wazo-phoned/conf.d/010-subnets.yml <<EOF
+debug: yes
+rest_api:
+  authorized_subnets: [$IP_authorize]
+EOF
+    echo -e "XXX\n50\nAdd IP... Done.\nXXX"
+    sleep 0.5
+
+    echo -e "XXX\n50\nRestart service... \nXXX"
+    systemctl restart wazo-phoned
+
+    echo -e "XXX\n100\nRestart service... Done.\nXXX"
+    sleep 0.5
+
+} | whiptail --gauge "Veuillez patienter" 6 60 0
+
+whiptail --title "Hello Wazo HELP / IP Authorize Subnet" --msgbox "$IP_authorize has been added to the authorize subnets" 10 60
+
 fi
 
-echo "Create/Update file ${1} ${2}"
-DEBUG="${2}"
-IP="${1}"
-touch /etc/wazo-phoned/conf.d/010-subnets.yml
-cat > /etc/wazo-phoned/conf.d/010-subnets.yml <<EOF
-debug: ${DEBUG}
-rest_api:
-  authorized_subnets: [${IP}]
-EOF
-echo "Done"
-echo "Restart service"
-systemctl restart wazo-phoned
-echo "Done, All is Good - Bye"
+}
+
+autorize_subnets
